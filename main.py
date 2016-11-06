739e852e7c5d8e6bf91674b6cc65af4ebc08dfc8
Avi <aks2203@columbia.edu>
2016-11-06 15:06:05 -0500

    _output folder added for organization of output figures.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import random
 from scipy.interpolate import lagrange
 import numpy as np
 import matplotlib.pyplot as plt
-from os import system
+import os
 import pdb
 
 #################################
@@ -32,22 +32,29 @@ bathymetry_data = np.array([(0,-20), (1,-18), (2, -16), (3, -16), (4, -12), (5, 
 
 grid_edges = np.array([0, 1, 2, 3, 4, 5, 6])
 
+if not os.path.exists('_output'):
+    os.makedirs('_output')
+else:
+    os.system('rm -rf _output/*')
+    print 'rm -rf _output/*'
+
+
 # initialize the grid object:
 my_grid = Grid(grid_edges, bathymetry_data)
 my_grid.fill_const(-15.5) 
 my_grid.eta2[:] = 0
 my_grid.plot_grid() # plot level one grid
-plt.savefig('figure1')
+plt.savefig('_output/figure1')
 print type(my_grid.grid)
 
 print my_grid.refine(np.array([0, 2, 4, 6]))
 my_grid.plot_grid() # plot level two grid
-plt.savefig('figure2')
+plt.savefig('_output/figure2')
 print type(my_grid.grid)
 
 print my_grid.refine(None, 2);
 my_grid.plot_grid() # plot level three grid
-plt.savefig('figure3')
+plt.savefig('_output/figure3')
 
 print type(my_grid.grid)
 print my_grid.grid
